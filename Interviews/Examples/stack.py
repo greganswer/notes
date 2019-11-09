05d4d24508c39fffd59ab59c612ea862e7ca5c3e
Greg Answer <greganswer@gmail.com>
2019-11-08 19:45:39 -0500

    Improve stack example

--- Interviews/Examples/stack.py
+++ Interviews/Examples/stack.py
@@ -1,102 +1,98 @@
-class Node(object):
+import unittest
+
+
+class Node:
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
-    def __str__(self):
-        next = None if self.next is None else self.next.data
-        return f"Node: {self.data}, Next: {next}"
 
-
-class Stack(object):
+class Stack:
     def __init__(self):
         self.top = None
-        self.bottom = None
         self.size = 0
 
-    def __str__(self):
-        current = self.top
-        output = ""
-        while current:
-            output += str(current.data)
-            if current == self.top:
-                output += ' * TOP'
-            if current == self.bottom:
-                output += ' * BOTTOM'
-            output += "\n"
-            current = current.next
-        return output
-
-    def peek(self):
-        return self.top
+    def __len__(self):
+        return self.size
 
     def push(self, data):
-        node = Node(data, next=self.top)
-        if self.bottom is None:
-            self.bottom = node
-        self.top = node
+        self.top = Node(data, self.top)
         self.size += 1
 
-    def pop(self):
-        if self.top is None:
-            return None
-        if self.top == self.bottom:
-            self.bottom = None
-        unwanted = self.top
-        self.top = unwanted.next
-        self.size -= 1
-        return unwanted
+    def peek(self):
+        if self.top:
+            return self.top.data
 
     def is_empty(self):
-        return self.size == 0
+        return not self.top
+
+    def pop(self):
+        if not self.top:
+            return
+        removed = self.top
+        self.top = removed.next
+        self.size -= 1
+        return removed.data
 
 
 class ListStack(object):
     def __init__(self):
         self.list = []
 
-    # TODO: Determine how to make this more readable for lists.
-    def __str__(self):
-        output = ""
-        for index, item in enumerate(reversed(self.list)):
-            output += str(item)
-            if index == 0:
-                output += ' * TOP'
-            if index == len(self.list)-1:
-                output += ' * BOTTOM'
-            output += "\n"
-        return output
+    def __len__(self):
+        return len(self.list)
 
     def peek(self):
-        return self.list[-1]
+        if self.list:
+            return self.list[-1]
 
     def push(self, data):
         self.list.append(data)
 
     def pop(self):
+        if not self.list:
+            return
         return self.list.pop()
 
     def is_empty(self):
         return len(self.list) == 0
 
 
-def test_stack(stack):
-    stack.push('Discord')
-    stack.push('Udemy')
-    stack.push('Google')
-    print(stack)
-    stack.pop()
-    print(stack)
-    print(stack.peek())
+class TestStack(unittest.TestCase):
+    """ Test the behavior of the Stack and ListStack classes. """
+
+    def setUp(self):
+        self.stacks = [Stack(), ListStack()]
+
+    def test_init(self):
+        for stack in self.stacks:
+            self.assertIsNone(stack.peek())
+            self.assertTrue(stack.is_empty())
+            self.assertEqual(len(stack), 0)
+
+    def test_push(self):
+        for stack in self.stacks:
+            stack.push(1)
+            self.assertEqual(stack.peek(), 1)
+            self.assertFalse(stack.is_empty())
+            self.assertEqual(len(stack), 1)
+
+    def test_pop(self):
+        for stack in self.stacks:
+            stack.push(1)
+            stack.pop()
+            self.assertIsNone(stack.peek())
+            self.assertTrue(stack.is_empty())
+            self.assertEqual(len(stack), 0)
+
+    def test_pop_when_stack_empty(self):
+        for stack in self.stacks:
+            stack.pop()
+            self.assertIsNone(stack.peek())
+            self.assertTrue(stack.is_empty())
+            self.assertEqual(len(stack), 0)
 
 
-def main():
-    print('\n........testing stack...........')
-    test_stack(Stack())
-    print('\n........testing list stack...........')
-    test_stack(ListStack())
-
-
-if __name__ == '__main__':
-    main()
-    print('done!')
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
